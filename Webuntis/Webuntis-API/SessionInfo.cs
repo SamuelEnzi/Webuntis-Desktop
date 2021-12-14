bd78a848ae3a26e11931831352ead5228488515b
ManamanaTheReal499 <62288386+ManamanaTheReal499@users.noreply.github.com>
2021-12-14 23:32:38 +0100

    api push

---
+++ Webuntis/Webuntis-API/SessionInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webuntis_API
+{
+    public class SessionInfo
+    {
+        public string JSessionID { get; set; }
+        public string Schoolname { get; set; }
+        public string TraceID { get; set; }
+        public string Auth { get; set; }
+        public string Token { get; set; }
+
+        public SessionInfo()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            var JsID = JSessionID != null ? $"JSESSIONID={JSessionID};" : string.Empty;
+            var ScID = Schoolname != null ? $"schoolname=\"{Schoolname}\";" : string.Empty;
+            var TrID = TraceID != null ?    $"traceId={TraceID};" :       string.Empty;
+            var Au = Auth != null ?         $"auth={Auth};" :             string.Empty;
+            return $"{JsID} {ScID} {TrID} {Au}";
+        }
+    }
+}
---
+++ Webuntis/WebuntisApiTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Webuntis_API;
+
+namespace WebuntisApiTest
+{
+    internal class Program
+    {
+        static WebuntisClient client;
+        static void Main(string[] args)
+        {
+            client = new WebuntisClient(new Secret("enz-sam", "AmanakoiAmana1!"));
+            bool success = client.Open();
+            if (!success) return;
+
+            var UserInfo = client.GetUserInfo();
+            var Lessions = client.GetLessions(UserInfo.user.person.id, UserInfo.currentSchoolYear.id);
+            var PMGrades = client.GetGrades(UserInfo.user.person.id, 24090); //24090 project management
+
+        }
+
+        
+    }
+}
