0f0cdae66b5aba71711e6b6c4ab30285d55db159
ManamanaTheReal499 <62288386+ManamanaTheReal499@users.noreply.github.com>
2021-12-14 20:37:11 +0100

    init

---
+++ Webuntis/Webuntis-Desktop/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Webuntis_Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Views.UserInterface ui = new Views.UserInterface();
+            ui.Show();
+        }
+    }
+}
