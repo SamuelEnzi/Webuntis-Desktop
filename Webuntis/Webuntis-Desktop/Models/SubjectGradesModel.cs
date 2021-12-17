5ccf6868b1cb27321b617d1384f88606f31d83e1
Tobias Leitner <tobias.leitner2002@gmail.com>
2021-12-17 18:10:36 +0100

    Grades DataGrid

---
+++ Webuntis/Webuntis-Desktop/Models/SubjectGradesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webuntis_Desktop.Models
+{
+    internal class SubjectGradesModel
+    {
+        public String Fach { get; set; }
+        public List<double> Noten { get; set; } = new List<double>();
+        public double Durchschnitt { get => Math.Round(Noten.Sum() / Noten.Count, 2); }
+        public double reachTarget { get => Math.Round(Zielnote * Noten.Count - Noten.Sum() + Zielnote, 2); }
+        public double Zielnote { get; set; }
+
+
+        public SubjectGradesModel() { }
+        public SubjectGradesModel(string subject, double target)
+        {
+            this.Fach = subject;
+            this.Zielnote = target;
+        }
+
+        public void AddMark(double mark)
+        {
+            Noten.Add(mark);
+        }
+    }
+}
--- Webuntis/Webuntis-Desktop/Modules/Votes.xaml.cs
+++ Webuntis/Webuntis-Desktop/Modules/Votes.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Webuntis_API;
+using Webuntis_Desktop.Models;
 
 namespace Webuntis_Desktop.Modules
 {
@@ -26,7 +27,46 @@ namespace Webuntis_Desktop.Modules
         public object Display(WebuntisClient client)
         {
             this.client = client;
-            this.Lable.Content = $"Votes:: {client.Secret.Username}";
+
+            Webuntis_API.Models.UserInfo.Root user = client.GetUserInfo();
+            Webuntis_API.Models.LessonInfo.Root lessons = user.GetLessons(client);
+            List<Webuntis_Desktop.Models.SubjectGradesModel> subjects = new List<SubjectGradesModel>();
+
+
+            UI_votesOutput.Columns.Add(new DataGridTextColumn() { Header = "Fach" });
+
+            lessons.data.lessons.ForEach(x =>
+            {
+                SubjectGradesModel subject = new SubjectGradesModel(x.subjects, 6);
+                subjects.Add(subject);
+                x.GetGrades(user, client).data.grades.ForEach(x =>
+                {
+                    subject.AddMark(x.mark.markDisplayValue);
+                });
+            });
+
+            for (int i = 0; i < subjects.Max(x => x.Noten.Count) || i < 8; i++)
+            {
+                UI_votesOutput.Columns.Add(new DataGridTextColumn());
+            }
+
+            UI_votesOutput.Columns.Add(new DataGridTextColumn() { Header = "Durchschnitt" });
+            UI_votesOutput.Columns.Add(new DataGridTextColumn() { Header = "zum erreichen benötigt" });
+            UI_votesOutput.Columns.Add(new DataGridTextColumn() { Header = "Zielnote" });
+
+            //UI_votesOutput.ItemsSource = subjects;
+            //UI_votesOutput.Items.Add(subjects.Noten);
+
+            subjects.ForEach(x =>
+            {
+                List<object> subject = new List<object> { x.Fach, x.Durchschnitt, x.reachTarget, x.Zielnote};
+                subject.AddRange(x.Noten.Cast<object>());
+                UI_votesOutput.Items.Add(subject);
+            });
+
+
+
+
             return this;
         }
     }
